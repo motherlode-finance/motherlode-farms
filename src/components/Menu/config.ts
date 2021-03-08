221dfa30a137b5521d75e22ff6d90377bba07d33
Motherlode Finance <motherlodefinance@protonmail.com>
2021-03-08 23:22:47 +1100

    Fix menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -53,18 +53,18 @@ const config: MenuEntry[] = [
         label: 'PancakeSwap',
         href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
       },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/motherlode-finance',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/motherlode-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
+      // {
+      //   label: 'CoinGecko',
+      //   href: 'https://www.coingecko.com/en/coins/motherlode-finance',
+      // },
+      // {
+      //   label: 'CoinMarketCap',
+      //   href: 'https://coinmarketcap.com/currencies/motherlode-finance/',
+      // },
+      // {
+      //   label: 'AstroTools',
+      //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      // },
     ],
   },
   {
@@ -77,29 +77,29 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Docs',
-        href: 'https://motherlode-finance.gitbook.io/motherlode-finance/',
+        href: 'https://motherlodefinance.gitbook.io/motherlode-finance/',
       },
       {
         label: 'Blog',
-        href: 'https://motherlodefinance.medium.com/',
+        href: 'https://medium.com/@motherlodefinance',
       },
     ],
   },
-  {
-    label: 'Partnerships/IFO',
-    icon: 'MotherlodeIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
-  },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.motherlode.finance/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/motherlode-finance',
-  },
+  // {
+  //   label: 'Partnerships/IFO',
+  //   icon: 'MotherlodeIcon',
+  //   href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  // },
+  // {
+  //   label: 'Audit by Hacken',
+  //   icon: 'AuditIcon',
+  //   href: 'https://www.motherlode.finance/files/hackenAudit.pdf',
+  // },
+  // {
+  //   label: 'Audit by CertiK',
+  //   icon: 'AuditIcon',
+  //   href: 'https://certik.org/projects/motherlode-finance',
+  // },
 ]
 
 export default config
