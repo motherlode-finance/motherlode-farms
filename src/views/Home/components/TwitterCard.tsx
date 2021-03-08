3192b679d2384aa84ffb122b07b9287557352d68
Motherlode Finance <motherlodefinance@protonmail.com>
2021-03-08 23:26:45 +1100

    Update twitter name

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'MotherlodeFinance'
+            screenName: 'MotherlodeDefi'
           }}
           options={{
             height: '300',
